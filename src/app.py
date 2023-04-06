a4bd1fc5a775d050a56f994a4298da959bcf185b
ChuchoAraujo <jeseazuara@gmail.com>
2023-04-06 19:13:19 +0000

    routes y models

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Post
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,49 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
+#----------------------------------------------------------------------------------------------------------------------------#
+##RUTAS USER-------------------------
+#GET post
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_users():
+    all_users = User.query.all()
+    serialize_users = [user.serialize() for user in all_users]
+    return jsonify(serialize_users ), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:id>', methods= ['GET'])
+def get_user(id):
+    print(id)
+    user = User.query.get(id)
+    return jsonify(user.serialize()), 200
 
-    return jsonify(response_body), 200
+
+#POST
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(email = data['email'], password = data['password'], username = data['username'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 200
+
+##RUTAS POST-------------------------
+
+@app.route('/post', methods=['POST'])
+def create_post():
+    data = request.get_json()
+    new_post = Post(title=data['title'], message=data['message'], user_id=data['user_id'])
+    db.session.add(new_post)
+    db.session.commit()
+    return jsonify(new_post.serialize()), 201
+
+@app.route('/post', methods=['GET'])
+def get_all_posts():
+    all_posts = Post.query.all()
+    serialize_posts = [user.serialize() for user in all_posts]
+    return jsonify(serialize_posts ), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -7,13 +8,55 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    post = db.relationship('Post', back_populates= 'user')
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def __init__(self, email, password, username):
+        self.email = email,
+        self.password = password,
+        self.username = username
+        self.is_active = True
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "active": self.is_active,
+            "post": [post.serialize_populate() for post in  self.post]
+            
             # do not serialize the password, its a security breach
+        }
+    def serialize_populate(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+        }
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    title = db.Column(db.String(80), nullable=False)
+    message =  db.Column(db.String(180), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', back_populates= 'post')
+
+    def __init__(self, title, message, user_id):
+        self.title = title,
+        self.message = message,
+        self.user_id = user_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "message": self.message,
+            "user_id": self.user_id,
+            "user": self.user.serialize_populate()
+        }
+    
+    def serialize_populate(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "message": self.message,
         }
